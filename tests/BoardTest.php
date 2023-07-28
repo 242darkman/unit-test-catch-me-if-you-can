1d5154315caef0c3ff9c5110786ba055aaca4eb1
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-28 18:04:52 +0200

    test(board): ajout d'un test unitaire pour la méthode `placePlayer`

--- tests/BoardTest.php
+++ tests/BoardTest.php
@@ -2,6 +2,7 @@
 require 'vendor/autoload.php';
 
 use App\Game\Board;
+use App\Game\Player;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -69,5 +70,17 @@ final class BoardTest extends TestCase
     $this->assertFalse($this->board->isValidPosition(-2, 5));
     $this->assertFalse($this->board->isValidPosition(5, 11));
     $this->assertFalse($this->board->isValidPosition(11, 11));
+  }
+  public function testPlacePlayer(): void
+  {
+    $player1 = new Player(1, 1, 'N');
+    $player2 = new Player(2, 2, 'N');
+
+    $this->board->placePlayer($player1, Board::PLAYER1);
+    $this->board->placePlayer($player2, Board::PLAYER2);
+
+    $grid = $this->board->getGrid();
+    $this->assertEquals(Board::PLAYER1, $grid[1][1], "Le joueur 1 doit être placé à la position (1, 1)");
+    $this->assertEquals(Board::PLAYER2, $grid[2][2], "Le joueur 2 doit être placé à la position (2, 2)");
   }
 }
