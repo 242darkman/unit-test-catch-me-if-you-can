09347b37914940ae755dd0b996931df99b5588e7
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-28 19:31:16 +0200

    test: ajout d'une méthode de test pour voir que notre jeu s'initialise bien et que les joueurs sont bien placés sur le plateau de jeu.

---
+++ tests/GameTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Game\Board;
+use App\Game\Game;
+use App\Game\Player;
+use PHPUnit\Framework\TestCase;
+
+final class GameTest extends TestCase
+{
+  private $player1;
+  private $player2;
+  private $game;
+
+  /**
+   * Prépare le test en créant les joueurs et le jeu
+   * 
+   * la méthode initialise les joueurs et le plateau de jeu
+   */
+  protected function setUp(): void
+  {
+    parent::setUp();
+    $this->player1 = new Player(5, 5, 'N');
+    $this->player2 = new Player(4, 4, 'S');
+    $this->game = new Game($this->player1, $this->player2);
+  }
+
+  public function testInitialValues(): void
+  {
+    $player1 = new Player(5, 5, 'N');
+    $player2 = new Player(4, 4, 'S');
+    $game = new Game($player1, $player2);
+    $this->assertInstanceOf(Player::class, $game->getPlayer1());
+    $this->assertInstanceOf(Player::class, $game->getPlayer2());
+    $this->assertInstanceOf(Board::class, $game->getBoard());
+
+    // on vérifie que les joueurs sont bien placés sur le plateau
+    $this->assertEquals(Board::PLAYER1, $game->getBoard()->getCellState($player1->getPosX(), $player1->getPosY()));
+    $this->assertEquals(Board::PLAYER2, $game->getBoard()->getCellState($player2->getPosX(), $player2->getPosY()));
+  }
+}
